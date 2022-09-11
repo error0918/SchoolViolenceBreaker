f831a849b27b0e894edab2504bc920f883b36870
error0918 <jtaeyeon05@gmail.com>
2022-09-12 03:41:44 +0900

    2022/09/12 03:41

--- app/src/main/java/com/taeyeon/schoolviolencebreaker/Helpful.kt
+++ app/src/main/java/com/taeyeon/schoolviolencebreaker/Helpful.kt
@@ -6,7 +6,9 @@ package com.taeyeon.schoolviolencebreaker
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -24,8 +26,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -35,12 +39,19 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.taeyeon.core.Core
+import com.taeyeon.core.Utils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
 
 object Helpful {
 
     data class Helpful(
         val imageBitmap: ImageBitmap,
+        val imageBitmapBackground: Color? = null,
         val title: String,
         val description: String,
         val link: String,
@@ -55,30 +66,88 @@ object Helpful {
             drawable.draw(canvas)
             bitmap.asImageBitmap()
         }
+        val getImageFromWeb = { link: String ->
+            var imageBitmap: ImageBitmap? = null
+            val thread = Thread {
+                imageBitmap = BitmapFactory.decodeStream(
+                    URL(link).openConnection().inputStream
+                ).asImageBitmap()
+            }
+            thread.start()
+            thread.join()
+            imageBitmap!!
+        }
 
         val todo = Helpful(
             imageBitmap = drawableToImageBitmap(R.drawable.ic_launcher_round),
             title = "a",
             description = """
-                     aaa
-                """.trimIndent(),
+                asdasd
+            """.trimIndent(),
             link = "https://naver.com/"
         )
         listOf(
             Helpful(
-                imageBitmap = drawableToImageBitmap(R.drawable.ic_launcher_round),
-                title = "학교폭력예방교육지원센터",
+                imageBitmap = getImageFromWeb("https://www.moe.go.kr/img/2021Renewal/content/mi_2_1.png"),
+                imageBitmapBackground = Color.White,
+                title = "교육부",
                 description = """
-                     학교폭력예방교육지원센터는 교육부의 지정을 받아 2018년 5월부터 한국청소년정책연구원의 부설센터로 운영되고 있습니다.
-                     본 센터는 학교폭력 예방교육 어울림 프로그램 개발·보급·운영 및 학교폭력 예방교육 지원체계 구축을 위해 어울림 프로그램의 재구조화, 학교폭력 예방 교과연계 원격연수 프로그램 개발, 학교폭력 예방교육 프로그램 적용 효과성 분석, 학교폭력 추세 대응을 위한 교육자료 제작 및 보급 그리고 학부모를 위한 학교폭력 예방 소식지 발간 등의 사업을 수행하고 있습니다.
-                     또한 학교폭력 예방을 위한 전문가 컨설팅 및 교육연수, 학생참여 학교폭력 예방활동 지원을 위한 어울림 학생서포터즈단 운영 그리고 시도 교육청 단위 학교폭력 예방 협의체 구성 및 단위 별 사업운영 지원을 통해 활발한 현장 중심의 맞춤형 사업을 수행하고 있습니다.
-                     본 센터는 학생들의 자발적 참여·활동을 기반으로 지역 주도의 학교폭력 예방활동과 함께 학교 교육과정에 초점을 맞추어 학생, 교사, 학부모 및 지역 사회가 함께 참여하는 학교폭력 예방교육 사업을 추진·수행하고 있습니다.
+                     대한민국의 인적자원 개발정책과 학교교육·평생교육 및 학술에 관한 사무를 관장하는 중앙행정기관.
                 """.trimIndent(),
-                link = "https://www.stopbullying.re.kr/mps"
+                link = "https://www.moe.go.kr/main.do?s=moe"
             ),
-            todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo, todo,
             Helpful(
-                imageBitmap = drawableToImageBitmap(R.drawable.ic_launcher_round),
+                imageBitmap = getImageFromWeb("https://www.police.go.kr/resources/common/images/sub/txt_kcc.png"),
+                imageBitmapBackground = Color.White,
+                title = "경찰청",
+                description = """
+                     대한민국의 국가경찰기관. 치안경찰에 관한 사무를 총괄하기 위해 행정안전부 장관 소속 아래에 둔 기관.
+                """.trimIndent(),
+                link = "https://www.police.go.kr/index.do"
+            ),
+            Helpful(
+                imageBitmap = getImageFromWeb("https://www.kyci.or.kr/userSite/images/common/logo.png"),
+                imageBitmapBackground = Color.White,
+                title = "한국청소년상담복지개발원",
+                description = """
+                     한국청소년상담복지개발원은 전국 600여개의 청소년상담복지센터, 학교밖청소년지원센터(청소년지원센터 꿈드림), 청소년복지시설(청소년쉼터, 청소년자립지원관, 청소년회복지원시설)을 총괄하는 중추 기관으로서 청소년의 건강한 성장과 행복한 꿈의 실현을 위해 다양한 상담복지사업을 수행하고 있는 여성가족부 산하 공공기관입니다.
+                     특히 학교 밖 청소년, 미디어 과의존 청소년 등 위기청소년 지원과 함께 청소년 정책연구 및 프로그램 개발, 상담복지 전문인력 양성 등 다양한 사업을 수행하고 있습니다.
+                """.trimIndent(),
+                link = "https://www.kyci.or.kr/userSite/index.asp"
+            ),
+            Helpful(
+                imageBitmap = getImageFromWeb("https://www.safe182.go.kr/static/home/new_images/logo.png"),
+                imageBitmapBackground = Color.White,
+                title = "안전Dream",
+                description = """
+                     경찰청은 해마다 증가하는 사회적 약자 대상 범죄에 대한 피해신고접수와 신속한 구조활동을 지원하기 위해, 기존 실종아동찾기 센터, 117학교·여성폭력 및 성매매피해자 긴급지원센터(117센터)등 관련 홈페이지를 「안전Dream」이라는 명칭으로 통합하였습니다.
+                     「안전Dream」은 다양한 신고서비스와 온라인 상담, 범죄예방 컨텐츠 등을 제공하며, 경찰청 내부업무시스템인 「프로파일링시스템」과의 연계로 보다 빠르고 신뢰성 있는 치안서비스를 제공하고 되었습니다.
+                """.trimIndent(),
+                link = "https://www.safe182.go.kr/index.do"
+            ),
+            Helpful(
+                imageBitmap = getImageFromWeb("https://www.btf.or.kr/images/common/logo02.png"),
+                imageBitmapBackground = Color.White,
+                title = "푸른나무재단",
+                description = """
+                     전국 학교폭력 상담전화 1588-9128(구원의팔)
+                     학교폭력으로 고통 받는 청소년, 부모님, 학교 선생님들께 푸른나무재단에서 "구원의 팔"을 내밀어 드립니다.
+                     푸른나무재단에서는 1995년부터 전국 학교폭력 상담전화 1588-9128(구원의팔)을 운영하고 있습니다. 학교폭력 피·가해 위기상담 및 심리·정서적 지원, 학교폭력 발생 시 대처 및 해결을 위한 조언, 전문 정보제공, 학교폭력 사안처리에 대한 고충 상담을 제공합니다.
+                """.trimIndent(),
+                link = "https://www.btf.or.kr"
+            ),
+            Helpful(
+                imageBitmap = getImageFromWeb("https://www.cyber1388.kr:447/images/common/logo_renew.png"),
+                imageBitmapBackground = Color.White,
+                title = "전청소년사이버상담센터",
+                description = """
+                     전청소년사이버상담센터는 지난 10여 년간 수십만 명의 청소년들과 함께 아래와 같이 성장해 왔습니다. 1999년, PC 보급과 인터넷 발전이라는 시대적 상황에 발맞춰 우리나라 사이버상담의 일대 도약의 문을 열게 되었습니다. 2011년, 청소년사이버상담센터의 개소로 보다 많은 청소년들이 이용할 수 있도록 접근성을 높였으며, 최근에는 누리소통망(SNS)의 확산과 비대면 일상생활의 사회적 현상을 고려한 찾아가는 상담(사이버아웃리치)과 카카오톡 및 문자 메시지상담까지 확대 운영하고 있습니다.
+                """.trimIndent(),
+                link = "https://www.cyber1388.kr:447"
+            ),
+            Helpful(
+                imageBitmap = getImageFromWeb("https://www.edunet.net/nedu/images/common_c/edunet_newlogo.png"),
+                imageBitmapBackground = Color.White,
                 title = "에듀넷·티-클리어",
                 description = """
                     에듀넷·티-클리어는 약자로 교육과정과 교육정책 전반의 정보를 통합 제공하고, 협업 소통을 지원하는 교육정보 통합 지원 서비스입니다.
@@ -103,6 +172,7 @@ object Helpful {
                 HelpfulUnit(
                     imageBitmap = helpful.imageBitmap,
                     imageBitmapDescription = helpful.title,
+                    imageBitmapBackground = helpful.imageBitmapBackground,
                     title = helpful.title,
                     description = helpful.description,
                     link = helpful.link,
@@ -120,6 +190,7 @@ object Helpful {
     fun HelpfulUnit(
         imageBitmap: ImageBitmap? = null,
         imageBitmapDescription: String? = null,
+        imageBitmapBackground: Color? = null,
         title: String,
         description: String,
         link: String,
@@ -175,9 +246,10 @@ object Helpful {
                                 imageWidth = intSize.width
                             }
                             .background(
-                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+                                color = imageBitmapBackground ?: MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
                                 shape = MaterialTheme.shapes.medium
                             )
+                            .padding(5.dp)
                     )
                 }
 
